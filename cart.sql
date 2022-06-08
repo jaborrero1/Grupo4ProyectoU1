f2e8285e07c812212b445db1202aafd886ce8b2e
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2022-06-08 07:28:25 -0500

    BDD actualizada

--- cart.sql
+++ cart.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-06-2022 a las 03:58:20
--- Versión del servidor: 10.4.19-MariaDB
--- Versión de PHP: 8.0.6
+-- Tiempo de generación: 08-06-2022 a las 14:23:12
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -53,6 +53,32 @@ INSERT INTO `contacto` (`id_codigo`, `nombreconta`, `apellidoconta`, `cedulacont
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `pago`
+--
+
+CREATE TABLE `pago` (
+  `id_pago` int(11) NOT NULL,
+  `num_tarjeta` int(20) NOT NULL,
+  `ven_tarjeta` date NOT NULL,
+  `cod_tarjeta` varchar(3) NOT NULL,
+  `país_pago` varchar(20) NOT NULL,
+  `calle_prin` varchar(50) NOT NULL,
+  `calle_sec` varchar(50) NOT NULL,
+  `cod_post` varchar(5) NOT NULL,
+  `telefono_pago` varchar(10) NOT NULL,
+  `correo_pag` varchar(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `pago`
+--
+
+INSERT INTO `pago` (`id_pago`, `num_tarjeta`, `ven_tarjeta`, `cod_tarjeta`, `país_pago`, `calle_prin`, `calle_sec`, `cod_post`, `telefono_pago`, `correo_pag`) VALUES
+(1, 7415296, '2026-02-02', '023', 'ecuador', 'Cuenca', 'Sola', '236', '032159484', 'khodihd@espe.edu.ec');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `products`
 --
 
@@ -88,6 +114,12 @@ ALTER TABLE `contacto`
   ADD PRIMARY KEY (`id_codigo`);
 
 --
+-- Indices de la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`id_pago`);
+
+--
 -- Indices de la tabla `products`
 --
 ALTER TABLE `products`
@@ -102,6 +134,12 @@ ALTER TABLE `products`
 --
 ALTER TABLE `contacto`
   MODIFY `id_codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `id_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
